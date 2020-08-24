0c1ae6b8edf2c39cda5edfb0d95cd77b7ed9a253
Theresa Tobollik <theresa.joana.tobollik@uni-weimar.de>
2020-08-24 18:07:23 +0200

    adding reader for user input from console

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const Path = require("./path")
 const InputReader = require("./inputReader")
 
+
+//creating new inputReader and path
 inputReader = new InputReader()
 
 let ashPath = new Path()
 
-ashPath.moveAlongPath(inputReader.readFromCommandLine())
-console.log(ashPath.pokemonCaught)
-
+//receiving user input, moving along path and providing output (number of caught pokemon)
+inputReader.readFromProvidedSource().then((input) => {
+    ashPath.moveAlongPath(input)
+    console.log(ashPath.pokemonCaught)
+})
--- src/inputReader.js
+++ src/inputReader.js
@@ -5,6 +5,30 @@ class InputReader {
         this.input = ''
     }
 
+    //depending on user input the path can be written in the command line or console
+    async readFromProvidedSource() {
+        if (process.argv.length > 2) {
+            return(this.readFromCommandLine())
+        } else {
+            return this.readFromConsole()
+        }
+    }
+
+    async readFromConsole() {
+        const promise = new Promise((resolve, reject) => {
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+              })
+              rl.question("Please enter Ashs Path (allowed letters 'N', 'E', 'S', 'O, '') and press enter: ", (answer) => {
+                this.input = answer 
+                rl.close()
+                resolve(answer)
+            })
+        })
+        
+        return promise
+    }
 
     readFromCommandLine() {
         this.input = process.argv[2]
